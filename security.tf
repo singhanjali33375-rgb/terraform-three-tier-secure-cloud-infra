e519af01b4ed6fd4f03d2cc0cbfe6fc04e4e02f0
Anjali Singh <singhanjali33375@gmail.com>
2026-02-03 11:43:42 +0530

    Add AWS security group for web access

---
+++ security.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
