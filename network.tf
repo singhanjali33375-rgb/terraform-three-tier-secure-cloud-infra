f6e6d557f903492971c50ae46ef530cec2977944
Anjali Singh <singhanjali33375@gmail.com>
2026-02-03 11:42:49 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
